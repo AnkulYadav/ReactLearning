21a1a29971e067ce8df5f341184ed65781719aad
Ankul Yadav <ankulyadav9001@gmail.com>
2024-01-10 02:33:36 +0530

    first commit

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,31 @@
+function App (){
+    return (
+        <div>
+            <h1>My First React App</h1>
+            <p>Welcome :)</p>
+        </div>
+    );
+}
+
+export default App;
+
+/* 
+11. Types of components
+There  are two types of components 
+1. Functional components
+2. Class components
+Functional component:
+1. Functional components are also called as stateless components.
+2. Functional components are used to display static data.
+3. Functional components are written as a function.
+4. Functional components Can use hooks for state and effects.
+5. Functional components are easy to write.
+6. Functional components are easy to test.
+
+Class component:
+1. Class components are also called as stateful components.(Can Manage State )
+2. Class components are used to display dynamic data.
+3. Lifecycle : Access to lifecycle methods.
+4. Verbose : more boilerplate code 
+5. Complex : Hard 
+*/
